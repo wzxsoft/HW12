ea54c8470a4e20949ca0cce3ce4fe1439cf1569c
chaoxu1990 <xc900411@gmail.com>
2015-11-23 01:38:02 -0800

    implement lc4_memory.c file excepting search_opcode function.

--- lc4_memory.c
+++ lc4_memory.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "lc4_memory.h"
 
-
-/* 
- * adds a new node to the end of a linked list pointed to by head 
+/*
+ * adds a new node to the end of a linked list pointed to by head
  */
-int add_to_list (row_of_memory** head, 
-		 short unsigned int address, 
-		 short unsigned int contents) 
+int add_to_list (row_of_memory** head,
+		 short unsigned int address,
+		 short unsigned int contents)
 {
 
 	/* allocate memory for a single node */
@@ -22,61 +22,116 @@ int add_to_list (row_of_memory** head,
 
 	/* return 0 for success, -1 if malloc fails */
 
+    row_of_memory* node = (row_of_memory*)malloc(sizeof(row_of_memory));
+    if(!(node)) return -1;
+    node->next = NULL;
+
+    node->address = address;
+    node->contents = contents;
+
+    if(head == NULL || *head == NULL)
+        *head = node;
+    else{
+        while( (*head) -> next != NULL) (*head) = (*head) -> next;
+        (*head) -> next = node;
+    }
+
 	return 0 ;
 }
 
 
-/* 
- * search linked list by address field, returns node if found 
+/*
+ * search linked list by address field, returns node if found
  */
-row_of_memory* search_address (row_of_memory* head, 
-			       short unsigned int address ) 
+row_of_memory* search_address (row_of_memory* head,
+			       short unsigned int address )
 {
 	/* traverse linked list, searching each node for "address"  */
 
 	/* return pointer to node in the list if item is found */
 
 	/* return NULL if list is empty or if "address" isn't found */
-	 
+    while(head != NULL)
+    {
+        if(head->address == address) return head;
+        head = head->next;
+    }
 	return NULL ;
 }
 
-/* 
- * search linked list by opcode field, returns node if found 
+/*
+ * search linked list by opcode field, returns node if found
  */
-row_of_memory* search_opcode  (row_of_memory* head, 
-				      short unsigned int opcode  ) 
+row_of_memory* search_opcode  (row_of_memory* head,
+				      short unsigned int opcode  )
 {
 	/* traverse linked list until node is found with matching opcode
 	   AND "assembly" field of node is empty */
 
-	/* return pointer to node in the list if item is found */
+	/* return pointer to node in the list of item is found */
 
 	/* return NULL if list is empty or if no matching nodes */
-	 
+
 	return NULL ;
 }
 
 
-void print_list (row_of_memory* head ) 
+void print_list (row_of_memory* head )
 {
 	/* make sure head isn't NULL */
 
 	/* print out a header */
 
-	/* traverse linked list, print contents of each node */	
+	/* traverse linked list, print contents of each node */
+    if(head == NULL) return;
+    printf("<address> \t <contents> \n");
+    while( head != NULL)
+    {
+        printf("%-9d \t %-9d\n", head->address, head->contents);
+        head = head -> next;
+    }
 
 	return ;
 }
 
-/* 
+/*
  * delete entire linked list
  */
-int delete_list    (row_of_memory** head ) 
+int delete_list    (row_of_memory** head )
 {
 	/* delete entire list node by node */
 	/* if no errors, set head = NULL upon deletion */
 
 	/* return 0 if no error, -1 for any errors that may arise */
+
+    while((*head) != NULL)
+    {
+        row_of_memory *node = (*head)->next;
+        free(*head);
+        *head = node;
+    }
 	return 0 ;
+}
+
+int main (int argc, char** argv) {
+	row_of_memory* memory = NULL ;
+    add_to_list(&memory,1,2);
+    add_to_list(&memory,3,4);
+    print_list(memory);
+
+    row_of_memory * found1 = search_address(memory, 1);
+    printf("%d\n", found1 == NULL?-1:found1->address);
+
+    row_of_memory * found2 = search_address(memory, 3);
+    printf("%d\n", found2 == NULL?-1:found2->address);
+
+    row_of_memory * found3 = search_address(memory, 5);
+    printf("%d\n", found3 == NULL?-1:found3->address);
+
+    delete_list(&memory);
+    print_list(memory);
+
+
+
+    return 0;
 }
--- lc4_memory.h
+++ lc4_memory.h
@@ -1,11 +1,14 @@
 #ifndef LC4_MEMORY_H
 #define LC4_MEMORY_H
 
-/*
- * definition of linked list node 
- */ 
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct row_of_memory_struct {
+/*
+ * definition of linked list node
+ */
+
+typedef struct row_of_memory {
 	short unsigned int address ;
 	char * label ;
 	short unsigned int contents ;
@@ -15,17 +18,17 @@ typedef struct row_of_memory_struct {
 
 
 /*
- * declaration of linked list helper functions 
+ * declaration of linked list helper functions
  */
 
-int add_to_list    	      (row_of_memory** head, 
-			       short unsigned int address, 
+int add_to_list    	      (row_of_memory** head,
+			       short unsigned int address,
 			       short unsigned int contents) ;
 
-row_of_memory* search_address (row_of_memory* head, 
+row_of_memory* search_address (row_of_memory* head,
 			       short unsigned int address ) ;
 
-row_of_memory* search_opcode  (row_of_memory* head, 
+row_of_memory* search_opcode  (row_of_memory* head,
 			       short unsigned int opcode  ) ;
 
 void print_list		      (row_of_memory* head ) ;
